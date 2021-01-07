2a1a1be1acc6a24b2a1e8ade8c05fbe97caf7f47
Pratik Pudage <77119181+pratikpudage@users.noreply.github.com>
2021-01-07 10:34:48 -0800

    Check SMB version 1 status on Windows Servers
    Server Message Block (SMB) is a network file sharing protocol meant for sharing files, printers, etc. between computers. There are three versions – Server Message Block (SMB) version 1 (SMBv1), SMB version 2 (SMBv2), and SMB version 3 (SMBv3). Microsoft recommends that you disable SMB1 for security reason.

---
+++ CheckSMBv1.ps1
@@ -0,0 +1,30 @@
+﻿<#
+ 
+.DESCRIPTION
+Use this script to check if SMBv1 is enabled on multiple computers.
+
+.NOTES
+Author: Pratik Pudage (PratikPudage@hotmail.com)
+
+.IMPORTANT
+Tested on Windows Server 2012 Operating System
+
+#>
+
+$Results = @()
+$Computers = Get-Content .\ServerList.txt
+
+    ForEach ($Computer In $Computers){
+$SMBv1 = Invoke-Command -ComputerName $Computer -ScriptBlock {Get-SmbServerConfiguration | Select EnableSMB1Protocol}
+  
+$Properties = @{
+                MachineName = $SMBv1.PSComputerName
+                SMBv1Enabled = $SMBv1.EnableSMB1Protocol
+  }   
+
+$Results += New-Object psobject -Property $properties
+  
+  }
+
+
+$Results |Select-Object MachineName, SMBv1Enabled |ft
